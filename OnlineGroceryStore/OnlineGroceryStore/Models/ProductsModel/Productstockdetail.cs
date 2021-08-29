7425d2b83152053ed7b9115863514990c4097933
Saburi8888 <saburi8888@gmail.com>
2021-08-29 11:36:29 +0530

    Add Models

---
+++ OnlineGroceryStore/OnlineGroceryStore/Models/ProductsModel/Productstockdetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace OnlineGroceryStore.Models
+{
+    public partial class Productstockdetail
+    {
+        public string Productname { get; set; }
+        public string Brandname { get; set; }
+        public int? Productprice { get; set; }
+        public int? Noofitemsavailable { get; set; }
+        public byte[] Productimage { get; set; }
+        public string Productcategory { get; set; }
+
+        public virtual Branddetail BrandnameNavigation { get; set; }
+    }
+}
